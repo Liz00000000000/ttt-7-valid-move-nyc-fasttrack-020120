ca0108cdf5947d66941b000e2de8b614d0a9f89b
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-02-04 14:37:49 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,7 +12,7 @@
 def valid_move? (board, index)
   #true = "Valid Move"
   #false = "Position Taken"
-  if board[index].between?(0,8) && board[index] == " "
+  if index.between?(0,8) && board[index] == " "
     return true
   else
     return false
