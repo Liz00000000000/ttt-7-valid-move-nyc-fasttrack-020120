aed414457221d5329ff11036e8c7e8c4246eaee4
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-02-04 14:11:03 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -13,9 +13,9 @@ def valid_move? (board, index)
   #true = "Valid Move"
   #false = "Position Taken"
   if index.between?(0,8) && board[index] == " "
-    puts "truthy value"
-  else
     puts "falsey value"
+  else
+    puts "truthy value"
   end
 end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
